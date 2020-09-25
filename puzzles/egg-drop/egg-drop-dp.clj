a173e656205ceee23977b5ff1634fd3f98bfdc9e
jmp3833 <jmpeterson132@gmail.com>
2020-09-25 19:15:44 -0400

    Docstrings and lets for clarity

--- puzzles/egg-drop/egg-drop-dp.clj
+++ puzzles/egg-drop/egg-drop-dp.clj
@@ -1,4 +1,7 @@
 (defn compute-dp [dp egg-row eggs] 
+  "given a precomputed vec of incrementing vals = to number of floors,
+  overwite values based on previously computed values of dp [i j],
+  or assign to static values if floor = 0 or eggs = 1"
   (reduce
     #(conj 
        % 
@@ -10,18 +13,24 @@
                  (apply 
                    max
                    (map (fn [x] 
-                          (min 
-                            (inc (get % (- %2 x)))
-                            (inc (get-in dp [(dec (dec eggs)) x]))))
+                          (let [egg-not-broken (get % (- %2 x))
+                                egg-broken (get-in dp [(dec (dec eggs)) x])]
+                            (min 
+                              (inc egg-broken)
+                              (inc egg-not-broken))))
                         xseq)))))
     []
     egg-row))
 
 (defn egg-drop-dp [eggs floors]
-  (reduce 
-    (fn [dp floors] 
-      (conj dp (compute-dp dp floors (inc (count dp)))))
-    [] 
-    (vec (repeat eggs (vec (range 0 (inc floors)))))))
+  "construct a matrix of size 'eggs by floors + 1'
+  and compute the egg-drop solution for each permulation"
+  (get-in 
+    (reduce 
+      (fn [dp floors] 
+        (conj dp (compute-dp dp floors (inc (count dp)))))
+      [] 
+      (vec (repeat eggs (vec (range 0 (inc floors))))))
+    [(dec eggs) floors]))
 
-(egg-drop-dp 3 14)
+(egg-drop-dp 2 100)
