231cdc83300e367a324303f96b70327a29caaa3a
jmp3833 <jmpeterson132@gmail.com>
2020-10-13 20:22:16 -0400

    Add support for minheap of objects

--- dst/heap.clj
+++ dst/heap.clj
@@ -1,62 +1,78 @@
 (defn parent-idx [idx] (int (. Math floor (/ (dec idx) 2))))
 
-(defn swap-until [heap idx] 
-  (if (<= idx 0) heap 
-    (let [parent-idx (parent-idx idx)
-          value (get heap idx) 
-          parent (get heap parent-idx)]
-      (cond 
-        (<= parent value) heap
-        (nil? parent-idx) heap
-        (zero? idx) heap
-        :else (recur 
-                (assoc (assoc heap parent-idx value) idx parent) 
-                parent-idx)))))
+(defn swap-until 
+  ([heap idx] (swap-until heap idx identity))
+  ([heap idx ex] 
+   (if (<= idx 0) heap 
+     (let [parent-idx (parent-idx idx)
+           value (get heap idx)
+           parent (get heap parent-idx)
+           value-ex (ex value)
+           parent-ex (ex parent)]
+       (cond 
+         (<= parent-ex value-ex) heap
+         (nil? parent-idx) heap
+         (zero? idx) heap
+         :else (recur 
+                 (assoc (assoc heap parent-idx value) idx parent) 
+                 parent-idx
+                 ex))))))
 
-(defn smallest-idx [heap idx]
+(defn smallest-idx 
+  ([heap idx] (smallest-idx heap idx identity))
+  ([heap idx ex]
   (let [left-idx (inc (* 2 idx))
         right-idx (inc left-idx)
         parent (get heap idx)
+        parent-ex (ex parent)
         local-heap [[parent idx] 
                     [(get heap left-idx) left-idx] 
                     [(get heap right-idx) right-idx]]]
     (reduce 
-      #(if (< (get %1 0) (get %2 0)) %1 %2)
+      #(if (< (ex (get %1 0)) (ex (get %2 0))) %1 %2)
       (filter 
         #(some? (get % 0)) 
-        local-heap))))
+        local-heap)))))
 
 (defn heapify 
-  ([heap] (heapify heap 0))
-  ([heap idx] 
+  ([heap ex] (heapify heap 0 ex))
+  ([heap idx ex] 
    (if (empty? heap) heap
-     (let [smallest (smallest-idx heap idx)
+     (let [smallest (smallest-idx heap idx ex)
            s-idx (get smallest 1)
            s-val (get smallest 0)]
        (if
          (= s-idx idx) heap
-         (heapify (assoc (assoc heap idx s-val) s-idx (get heap idx)) s-idx))))))
+         (recur
+           (assoc (assoc heap idx s-val) s-idx (get heap idx)) 
+           s-idx
+           ex))))))
 
-(defn add [heap ele] 
-  (if 
-    (not (vector? heap)) "please, vectors only" 
-    (let [appended (conj heap ele)]
-      (if-let [p-idx (parent-idx (dec (count appended)))]
-        (swap-until appended (dec (count appended)))
-        appended))))
+(defn add 
+  ([heap ele] (add heap ele identity))
+  ([heap ele ex] 
+   (if 
+     (not (vector? heap)) "please, vectors only" 
+     (let [appended (conj heap ele)]
+       (if-let [p-idx (parent-idx (dec (count appended)))]
+         (swap-until appended (dec (count appended)) ex)
+         appended)))))
 
-(defn delete [heap] 
+(defn delete [heap ex] 
   (let [lst (dec (count heap))
         swap (assoc heap 0 (get heap lst))]
-    (heapify (vec (take lst swap)))))
+    (heapify (vec (take lst swap)) ex)))
 
-(defn sort [heap result]
-  (println "heap" heap "result" result)
-  (let [ele (first heap)
-        modified (delete heap)
-        r (conj result ele)]
-    (if 
-      (empty? modified) r
-      (recur modified r))))
-
-(sort (reduce add [] [3 0 1 0 -500]) [])
+(defn hsort 
+  ([heap] (hsort heap [] identity))
+  ([heap ex] (hsort heap [] ex))
+  ([heap result ex]
+   (assert 
+     (number? (ex (first heap))) 
+     "requires non-empty heap with numeric value extracted by ex")
+   (let [ele (first heap)
+         modified (delete heap ex)
+         r (conj result ele)]
+     (if 
+       (empty? modified) r
+       (recur modified r ex)))))
--- puzzles/merge-lists/merge-n-lists.clj
+++ puzzles/merge-lists/merge-n-lists.clj
@@ -1,11 +1,26 @@
 (load-file "../../dst/heap.clj")
 
-(defn merge-w-limit [limit result lists]
-  (let [minheap
-        (reduce 
-          (fn [heap l] (add heap (first l))) 
-          [] 
-          lists)] 
-    minheap))
+(defn merge-w-limit 
+  ([lists limit]
+   (let [firsts (map-indexed (fn [i lst] [i (first lst)]) lists)
+         nexts (mapv rest lists)]
+     (merge-w-limit 
+       nexts
+       limit
+       (reduce 
+         (fn [coll ele] (add coll {:i (get ele 0) :v (get ele 1)} :v))
+         []
+         firsts) 
+       [])))
 
-(merge-w-limit 2 [] [[-1 4 7] [0 3 11] [-100 4 5]]) 
+  ([lists limit minheap result] 
+   (cond 
+     (empty? minheap) result
+     (= limit (count result)) result
+     :else (let [f (first minheap)
+                 rlocal (conj result (first minheap))
+                 hlocal (delete minheap :v)
+                 nxt (get lists (:i f))]
+             lists))))
+
+(merge-w-limit '[(-1 4 7) (0 3 11) (-100 4 5)] 2) 
