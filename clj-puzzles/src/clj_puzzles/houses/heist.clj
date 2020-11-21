c1a71eca945c9e4a3dd0b336c15e91bfe4244956
jmp3833 <jmpeterson132@gmail.com>
2020-11-20 19:12:13 -0500

    Bind solution to memoized form

--- clj-puzzles/src/clj_puzzles/houses/heist.clj
+++ clj-puzzles/src/clj_puzzles/houses/heist.clj
@@ -11,21 +11,20 @@
 
 (declare memo)
 
-(defn btree-heist 
-  ([tree] (btree-heist tree 0))
-  ([tree idx memo]
-   ;need to assoc in memo for each call
-   (cond 
-     (>= idx (count tree)) 0 
-     (contains? memo idx) (get memo idx)
-     :else 
-     (let [part (partial btree-heist tree)
-           l (part (lindex idx))
-           r (part (rindex idx))
-           ll (part (lindex (lindex idx)))
-           rr (part (rindex (rindex idx)))
-           lr (part (lindex (rindex idx)))
-           rl (part (rindex (lindex idx)))]
-       (max 
-         (+ (n-to-0 (get tree idx)) ll rr lr rl)
-         (+ 0 l r))))))
+(def btree-heist 
+  (memoize 
+    (fn [tree idx]
+      (assert (vector? tree) "tree must be a vector")
+      (cond 
+        (>= idx (count tree)) 0 
+        :else 
+        (let [part (partial btree-heist tree)
+              l (part (lindex idx))
+              r (part (rindex idx))
+              ll (part (lindex (lindex idx)))
+              rr (part (rindex (rindex idx)))
+              lr (part (lindex (rindex idx)))
+              rl (part (rindex (lindex idx)))]
+          (max 
+            (+ (n-to-0 (get tree idx)) ll rr lr rl)
+            (+ 0 l r)))))))
