e8cbf780df7e7ed2a1f5744fa332d63c61fd7436
jmp3833 <jmpeterson132@gmail.com>
2020-11-21 18:18:35 -0500

    Memoize using a raw atom. Neat stuff\!

--- clj-puzzles/src/clj_puzzles/houses/heist.clj
+++ clj-puzzles/src/clj_puzzles/houses/heist.clj
@@ -11,20 +11,23 @@
 
 (declare memo)
 
-(def btree-heist 
-  (memoize 
-    (fn [tree idx]
-      (assert (vector? tree) "tree must be a vector")
-      (cond 
-        (>= idx (count tree)) 0 
-        :else 
-        (let [part (partial btree-heist tree)
-              l (part (lindex idx))
-              r (part (rindex idx))
-              ll (part (lindex (lindex idx)))
-              rr (part (rindex (rindex idx)))
-              lr (part (lindex (rindex idx)))
-              rl (part (rindex (lindex idx)))]
-          (max 
-            (+ (n-to-0 (get tree idx)) ll rr lr rl)
-            (+ 0 l r)))))))
+(defn btree-heist 
+  ([tree] (btree-heist tree (atom {}) 0))
+  ([tree memo idx]
+   (assert (vector? tree) "tree must be a vector")
+   (cond 
+     (contains? @memo idx) (get @memo idx)
+     (>= idx (count tree)) 0 
+     :else 
+     (let [part (partial btree-heist tree memo)
+           l (part (lindex idx))
+           r (part (rindex idx))
+           ll (part (lindex (lindex idx)))
+           rr (part (rindex (rindex idx)))
+           lr (part (lindex (rindex idx)))
+           rl (part (rindex (lindex idx))) 
+           value (max 
+                   (+ (n-to-0 (get tree idx)) ll rr lr rl)
+                   (+ 0 l r))]
+       (swap! memo assoc idx value)
+       value))))
