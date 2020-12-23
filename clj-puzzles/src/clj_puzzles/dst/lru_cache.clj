4a1f69152317fc983b161aba54a47b0164f8f4c5
jmp3833 <jmpeterson132@gmail.com>
2020-12-23 17:27:52 -0500

    Switch to holding cache state in hashmap

--- clj-puzzles/src/clj_puzzles/dst/lru_cache.clj
+++ clj-puzzles/src/clj_puzzles/dst/lru_cache.clj
@@ -1,13 +1,16 @@
 (ns clj-puzzles.dst.lru-cache)
 
 (defn hash-dll-add!
-  ([[l m] k v] 
-   (let [newnode (ref {:prev nil :next nil :data v})]
+  ([cache k v] 
+   (let [l (:list cache)
+         m (:map cache)
+         newnode (ref {:prev nil :next nil :data v})]
      (dosync
        (if (nil? @l)
          (do 
            (ref-set l @newnode)
            (ref-set m {k l})
+           (ref-set (:last cache) l)
            nil)
          (do 
            (ref-set l (assoc @l :prev newnode))
@@ -15,8 +18,10 @@
            (ref-set m (assoc @m k newnode))
            nil))))))
 
-(defn hash-dll-del! [[l m] k]
-  (let [node (get @m k)
+(defn hash-dll-del! [cache k]
+  ;TODO check first and update last (if deleted)
+  (let [m (:map cache)
+        node (get @m k)
         prev (:prev @node)
         nxt (:next @node)]
     (dosync 
@@ -25,25 +30,28 @@
       nil)))
 
 (defn init! [size]
-  [(ref nil) (ref nil) size])
+  {:list (ref nil) :map (ref nil) :last {ref nil} :size size})
 
-(defn get! [[l m s] k f]
+(defn get! [cache k f]
   "Get element from cache at [k]ey, 
   populating the cache with [f]allback otherwise.
-  if cache is at maximum [s]ize, evicts least recently used item"
-  (if (contains? @m k) 
-    (let [v (get @m k)]
-      (when (some? (:prev @v))
-        (hash-dll-del! [l m] k)
-        (hash-dll-add! [l m] k (:data @v)))
-      (:data @v))
-    (let [v (f)]
-      (hash-dll-add! [l m] k v)
-      ;check size and delete here
-      v)))
+  if cache is at maximum size, evicts least recently used item"
+  (let [m (:map cache)
+        l (:list cache)]
+    (if (contains? @m k) 
+      (let [v (get @m k)]
+        (when (some? (:prev @v))
+          (hash-dll-del! cache k)
+          (hash-dll-add! cache k (:data @v)))
+        (:data @v))
+      (let [v (f)]
+        (hash-dll-add! cache k v)
+        ;TODO honor size and delete final ele here (if required)
+        v))))
 
 (comment 
-  (set! *print-level* 3)                             ; recursive data structure
+  (set! *print-level* 3)                             ; recursive data structure :'(
+
   (def c (cache/init 3))                             ; []
   (cache/get! c 1 (fn [] 10))                        ; [[1 10]]
   (cache/get! c 1 (fn [] "cache hit! not invoked"))  ; [[1 10]]
