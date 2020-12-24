d868beff3a7abc12892b28932544d3f7d10c10cc
jmp3833 <jmpeterson132@gmail.com>
2020-12-24 18:23:44 -0500

    Overwrite head of list in cache reference

--- clj-puzzles/src/clj_puzzles/dst/lru_cache.clj
+++ clj-puzzles/src/clj_puzzles/dst/lru_cache.clj
@@ -1,51 +1,64 @@
 (ns clj-puzzles.dst.lru-cache)
 
+(defn str-list 
+  ([l] (str-list l ""))
+  ([l sb] 
+   (let [node @l
+         v (str sb (:data node))]
+     (if (nil? (:next node)) 
+       v
+       (recur (:next node) (str v "<->"))))))
+
 (defn hash-dll-add!
-  ([cache k v] 
-   (let [l (:list cache)
-         m (:map cache)
+  [cache k v] 
+   (let [l (:list @cache)
+         m (:map @cache)
          newnode (ref {:prev nil :next nil :data v})]
      (dosync
+       (ref-set cache (assoc @cache :list newnode))
        (if (nil? @l)
          (do 
            (ref-set l @newnode)
            (ref-set m {k l})
-           (ref-set (:last cache) l)
-           nil)
+           (ref-set (:last @cache) l))
          (do 
            (ref-set l (assoc @l :prev newnode))
            (ref-set newnode (assoc @newnode :next l))
-           (ref-set m (assoc @m k newnode))
-           nil))))))
+           (ref-set m (assoc @m k newnode))))))
+  nil)
 
 (defn hash-dll-del! [cache k]
-  ;TODO check first and update last (if deleted)
-  (let [m (:map cache)
+  (let [l (:list @cache)
+        m (:map @cache)
         node (get @m k)
         prev (:prev @node)
-        nxt (:next @node)]
+        nxt (:next @node)
+        lst (:last @node)]
     (dosync 
-      (ref-set prev (assoc @prev :next nxt))
       (ref-set m (dissoc @m k))
+      (cond 
+        (some? prev) (ref-set prev (assoc @prev :next nxt))
+        (nil? nxt) (ref-set lst (:prev @lst))
+        :else (ref-set l (:next @l)))
       nil)))
 
 (defn init! [size]
-  {:list (ref nil) :map (ref nil) :last {ref nil} :size size})
+  (ref {:list (ref nil) :map (ref nil) :last (ref nil) :size size}))
 
-(defn get! [cache k f]
+(defn get! [c k f]
   "Get element from cache at [k]ey, 
   populating the cache with [f]allback otherwise.
   if cache is at maximum size, evicts least recently used item"
-  (let [m (:map cache)
-        l (:list cache)]
+  (let [m (:map @c)
+        l (:list @c)]
     (if (contains? @m k) 
       (let [v (get @m k)]
         (when (some? (:prev @v))
-          (hash-dll-del! cache k)
-          (hash-dll-add! cache k (:data @v)))
+          (hash-dll-del! c k)
+          (hash-dll-add! c k (:data @v)))
         (:data @v))
       (let [v (f)]
-        (hash-dll-add! cache k v)
+        (hash-dll-add! c k v)
         ;TODO honor size and delete final ele here (if required)
         v))))
 
