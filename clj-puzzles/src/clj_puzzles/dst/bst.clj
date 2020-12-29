7e4b63b3983b30d3d580630acdaeab4193ff7612
jmp3833 <jmpeterson132@gmail.com>
2020-12-28 21:01:19 -0500

    TODO: Partially through BST delete

--- clj-puzzles/src/clj_puzzles/dst/bst.clj
+++ clj-puzzles/src/clj_puzzles/dst/bst.clj
@@ -4,7 +4,6 @@
   ([data] (insert nil data))
   ([tree data] 
    (let [newnode {:l nil :r nil :d data}]
-     (println (:d tree))
      (if-let [head (:d tree)]
        (cond
          (and (<= data head) (nil? (:l tree))) (assoc tree :l newnode)
@@ -12,4 +11,21 @@
          (<= data head) (assoc tree :l (insert (:l tree) data))
          :else (assoc tree :r (insert (:r tree) data)))
        newnode))))
+
+(defn delete-subtree-min [tree]
+  (if-let [l (:l tree)] 
+    (if-let [ll (:l l)]
+      (assoc l :l (delete-subtree-min l))
+      (assoc tree :l nil)) ;how to extract min value here?
+      nil))
+
+(defn delete [tree data]
+  (if-let [head (:d tree)]
+    (cond
+      (and (= data head) (nil? (:l tree)) (nil? (:r tree))) nil
+      (and (= data head) (nil? (:l tree))) (assoc tree :l nil :r nil :d (get-in tree [:l :d])) 
+      (and (= data head) (nil? (:r tree))) (assoc tree :l nil :r nil :d (get-in tree [:r :d])) 
+      (= data head) (let [])
+      :else nil)))
+      ;grab smallest element from right subtree and replace with node to delete
 
