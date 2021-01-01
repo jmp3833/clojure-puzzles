79692d84c61da5808d0b776047e5c627ad11ca23
jmp3833 <jmpeterson132@gmail.com>
2021-01-01 14:38:39 -0500

    ngn circular array implementation

--- clj-puzzles/src/clj_puzzles/next_greater_number/ngn.clj
+++ clj-puzzles/src/clj_puzzles/next_greater_number/ngn.clj
@@ -7,7 +7,7 @@
   ([nums] (ngn nums (dec (count nums)) '()))
   ([nums i stack] 
    (assert (vector? nums) "nums must be a vec of int")
-   (assert (seq? nums) "stack must be seqable")
+   (assert (seq? stack) "stack must be seqable")
    (if-let [n (get nums i)]
      (if-let [top (first stack)]
        (if
@@ -17,12 +17,12 @@
      nums)))
 
 (defn ngn-delta 
-  "similar to ngn, but comutes the delta of the indices 
+  "similar to ngn, but computes the delta of the indices 
   between the current element and its ngn. Zero if no ngn exists"
   ([nums] (ngn-delta nums (dec (count nums)) '()))
   ([nums i stack] 
    (assert (vector? nums) "nums must be a vec of int")
-   (assert (seq? nums) "stack must be seqable")
+   (assert (seq? stack) "stack must be seqable")
    (if-let [n (get nums i)]
      (if-let [[top-ele top-idx] (first stack)]
        (if
@@ -31,6 +31,23 @@
        (ngn-delta (assoc nums i 0) (dec i) (cons [n i] stack)))
      nums)))
 
+(defn ngn-circular
+  "similar to ngn, but with a circular array."
+  ([nums] 
+   (let [n (count nums)] (ngn-circular nums (dec (* 2 n)) '() n (into [] (repeat n 0)))))
+  ([nums i stack len result] 
+   (if (< i 0) 
+     result
+     (let [idx (mod i len)
+           n (get nums idx)]
+       (if-let [top (first stack)]
+         (if
+           (<= top n) 
+           (ngn-circular nums i (rest stack) len result) 
+           (ngn-circular nums (dec i) (cons n stack) len (assoc result idx (first stack)))) 
+         (ngn-circular nums (dec i) (cons n stack) len (assoc result idx nil)))))))
+
 (comment 
   (assert (= (ngn '[2 1 2 4 3]) '[4 2 4 nil nil]))
-  (assert (= (ngn/ngn-delta '[73 74 75 71 69 72 76 73]) '[1 1 4 2 1 1 0 0])))
+  (assert (= (ngn-delta '[73 74 75 71 69 72 76 73]) '[1 1 4 2 1 1 0 0]))
+  (assert (= (ngn-circular '[2 1 2 4 3]) '[4 2 4 nil 4])))
